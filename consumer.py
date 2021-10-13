d065e34225d21a96d95db2ea2dba622a9e66716d
Javier García San Vicente <javigarciasv@gmail.com>
2021-10-13 20:30:03 +0200

    Consumer and producer created

---
+++ consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+    'numtest',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id=None
+    )
+
+for message in consumer:
+    message = message.value
+    print(message)
