94312da19da7596532108a31368bb1c0bb2456f8
kafka_user <kafka@LAPTOP-VUB3C91H.localdomain>
2021-10-19 17:22:09 +0200

    added small stuff

--- consumer.py
+++ consumer.py
@@ -2,7 +2,7 @@ from kafka import KafkaConsumer
 from json import loads
 
 consumer = KafkaConsumer(
-    'numtest',
+    'lyricgen',
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
