46f999661dbd60638c4a9711deb60811c4ea7833
kafka_user <kafka@LAPTOP-VUB3C91H.localdomain>
2021-10-19 22:42:05 +0200

    scraper can now get simple requests

--- scraper.py
+++ scraper.py
@@ -1,17 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-query_url = "https://www.rentanadviser.com/subtitles/subtitles4songs.aspx?"
-results_url = "https://www.rentanadviser.com/subtitles/getsubtitle.aspx?artist=Justin%20Bieber&song=Baby%20ft.%20Ludacris"
-
-artist = ["Kanye West"]
-song = "Good Morning"
-
-def parse_timestamps(lyrics):
-    #function that takes lyrics of a song as a string with format [ss:ms:ms]Text[ss:ms:ms]Text[ss:ms:ms]Text, and returns a list of [time, text]
-    lyrics = [lyrics.split("]") for i in lyrics]
-    #lyrics = 
-    
 
 def parse_song(artist, song):
     a_list = []
@@ -24,26 +13,46 @@ def parse_song(artist, song):
     return a_list[0], s
     
 def get_song(artist, song):
-    #Function to get the lyrics from the results page
+    #Function to get the lyrics from the results page given artist and song
     results_url = f"https://www.rentanadviser.com/subtitles/getsubtitle.aspx?artist={artist}&song={song}"
     print(results_url)
+    
     r = requests.get(results_url)
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    #print(soup.prettify())
-    lyrics = soup.find(id="ctl00_ContentPlaceHolder1_lbllyrics_simple").text
-    
-    
-    print(lyrics)
-    
-    
-    pass
+    try:
+        soup = BeautifulSoup(r.text, "html.parser")
+    except:
+        #! Not done yet
+        print("No results found")
+        lyrics = query_song(artist, song)
+        return None
+    else:
+        #print(soup.prettify())
+        lyrics = soup.find(id="ctl00_ContentPlaceHolder1_lbllyrics_simple").text
+        
+        #process it a little
+        lyrics = lyrics.split("[")
+        lyrics = {s[0]:s[1] for s in [l.split("]") for l in lyrics[1:]]}
+        return lyrics
 
 def query_song(artist, song):
-    pass
-page = requests.get(results_url)
+    #Function to query the website for the results page given artist and song when we cant find it right away
+    #! in progress
+    search = f"{artist}%20{song}"
+    query_url = f"https://www.rentanadviser.com/subtitles/subtitles4songs.aspx?q={search}"
+    r = requests.get(query_url)
+    soup = BeautifulSoup(r.text, "html.parser")
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    lyrics = soup.find(id="tablecontainer")
+    print(lyrics)
+    return lyrics
 
-a,s = parse_song(artist, song)
-get_song(a,s)
+def main():
+    artist = ["beyonce"] #example artist
+    song = "formation" #example song
+    artist, song = parse_song(artist, song)
+    lyrics = get_song(artist, song)
+    print(lyrics)
+
+
+if __name__ == "__main__":
+    main()
