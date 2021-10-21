2d2d98e9cd9cc2a34a03160b87d18a58ac9a57a1
kafka_user <kafka@LAPTOP-VUB3C91H.localdomain>
2021-10-21 11:50:41 +0200

    added query if regular dont work

--- scraper.py
+++ scraper.py
@@ -12,29 +12,31 @@ def parse_song(artist, song):
     s = "%20".join(song.lower().split(" "))
 
     return a_list[0], s
+
+def parse_lyrics(soup_object):
+    #print(soup.prettify())
+    lyrics = soup_object.find(id="ctl00_ContentPlaceHolder1_lbllyrics_simple").text
     
+    #process it a little
+    lyrics = lyrics.split("[")
+    lyrics = {s[0]:s[1] for s in [l.split("]") for l in lyrics[1:]]}
+    lyrics = lyrics.items()
+    return lyrics
+
 def get_song(artist, song)-> list():
     #Function to get the lyrics from the results page given artist and song
     results_url = f"https://www.rentanadviser.com/subtitles/getsubtitle.aspx?artist={artist}&song={song}"
     #print(results_url)
     
     r = requests.get(results_url)
-    try:
-        soup = BeautifulSoup(r.text, "html.parser")
-    except:
-        #! Not done yet
-        print("No results found")
-        lyrics = query_song(artist, song)
-        return None
+    #! just need to catch when this dont work
+    if r == None:
+        return query_song(artist, song)
     else:
-        #print(soup.prettify())
-        lyrics = soup.find(id="ctl00_ContentPlaceHolder1_lbllyrics_simple").text
+        return parse_lyrics(BeautifulSoup(r.text, "html.parser"))
+    
+
         
-        #process it a little
-        lyrics = lyrics.split("[")
-        lyrics = {s[0]:s[1] for s in [l.split("]") for l in lyrics[1:]]}
-        lyrics = lyrics.items()
-        return lyrics
 
 def query_song(artist, song)-> list():
     #Function to query the website for the results page given artist and song when we cant find it right away
@@ -44,21 +46,39 @@ def query_song(artist, song)-> list():
     r = requests.get(query_url)
     soup = BeautifulSoup(r.text, "html.parser")
 
-    lyrics = soup.find(id="tablecontainer")
-    print(lyrics)
+    table = soup.find("table")
+    rows = table.find_all('tr')
+    output = []
+    for row in rows:
+        cols=row.find_all('td')
+        cols=[x for x in cols]
+        link = cols[0].a['href']
+        link = f"https://www.rentanadviser.com/subtitles/{link}"
+        if "official" in link.lower(): #only want the official version
+
+            return parse_lyrics(BeautifulSoup(requests.get(link), "html.parser"))
+        else:
+            output.append(link) #if we dont find an official we just append it
     
     # //*[@id="tablecontainer"]/table/tbody/tr[1]/td/a
+    # if we didnt find any  official we just return the first link through parse_lyrics
 
-    return lyrics
+    return parse_lyrics(BeautifulSoup(requests.get(link).text, "html.parser"))
 
 def main():
     #testing
-    artist = ["beyonce"] #example artist
-    song = "formation" #example song
+    artist = ["justin bieber"] #example artist
+    song = "baby" #example song
     artist, song = parse_song(artist, song)
-    lyrics = query_song(artist, song)
-    #print(lyrics)
+    lyrics = get_song(artist, song)
+    print(lyrics)
 
 
 if __name__ == "__main__":
     main()
+
+    """
+    export PYSPARK_PYTHON=python3                                                                                           export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.7-src.zip:$PYTHONPATH                             export PATH=$SPARK_HOME/bin:$SPARK_HOME/python:$PATH
+    export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.7-src.zip:$PYTHONPATH                             export PATH=$SPARK_HOME/bin:$SPARK_HOME/python:$PATH
+    export PATH=$SPARK_HOME/bin:$SPARK_HOME/python:$PATH
+    """
